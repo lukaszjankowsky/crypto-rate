67359766eb346ac201c7df4d408c29f7a35ddaee
lukaszjankowsky <lukaszjankowsky@gmail.com>
2019-07-08 23:31:47 +0200

    Updates

---
+++ precache-manifest.597f2d5e634ff09950c922ed4b4292c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da7405f993f9f5837a7c218747a99115",
+    "url": "/crypto-rate/index.html"
+  },
+  {
+    "revision": "841d972ec47c5854d3bf",
+    "url": "/crypto-rate/static/css/main.e83e4c91.chunk.css"
+  },
+  {
+    "revision": "73d4bd57d2948c9e60cd",
+    "url": "/crypto-rate/static/js/2.b2ca986f.chunk.js"
+  },
+  {
+    "revision": "841d972ec47c5854d3bf",
+    "url": "/crypto-rate/static/js/main.fefe091f.chunk.js"
+  },
+  {
+    "revision": "e2a84902c547dfd9bb9c",
+    "url": "/crypto-rate/static/js/runtime~main.bb15693c.js"
+  }
+]);
